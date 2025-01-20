bec1b8b8ecf485a34235573062360506ec2cee7f
Ben Rucker <brucker117@gmail.com>
2025-01-20 15:01:53 -0800

    Remove "Extras" header

--- src/app/dub/components/DubEpisodes.tsx
+++ src/app/dub/components/DubEpisodes.tsx
@@ -44,7 +44,6 @@ export const DubEpisodes = React.memo<WithClassName>(function DubEpisodesFn({
             </Container>
           )),
           <Container key={SeasonName.EXTRAS} className="mt-1">
-            <h3>{CATALOG.seasons[SeasonName.EXTRAS].name}</h3>
             <Row>
               {sortEpisodes(
                 CATALOG.seasons[SeasonName.EXTRAS].episodes,
